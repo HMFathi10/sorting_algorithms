6df4cc936fde2a09e0ebdbc26ef53d271392c4ea
HMFathi10 <hossamfci10@gmail.com>
2023-12-18 14:00:35 +0200

    Added

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,19 +13,19 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	bool sorted = false;
+	int sorted = 0;
 
 	if (!array || size == 0)
 		return;
 
 	for (i = 0; i < size - 1 && !sorted; i++)
 	{
-		sorted = true;
+		sorted = 1;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				sorted = false;
+				sorted = 0;
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
