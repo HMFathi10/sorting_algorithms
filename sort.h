025e1554e735148cef8ec58552d18f7bd0e098d8
Abubakr Nasir <48620096+DevBekoDev@users.noreply.github.com>
2023-12-20 04:27:44 +0300

    Updated sort.h

--- sort.h
+++ sort.h
@@ -34,6 +34,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_ints(int *a, int *b);
 int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
 
 
 /*Advanced Tasks*/
